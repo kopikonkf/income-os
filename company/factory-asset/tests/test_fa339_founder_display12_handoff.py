38f26b59ebfa8b63156a683fc32f35a4214ea6e2
kopikonkf <kopiko.nkf@gmail.com>
2026-09-18 11:13:44 +0700

    Seal FA-339 Founder-visible browser operability

--- company/factory-asset/tests/test_fa339_founder_display12_handoff.py
+++ company/factory-asset/tests/test_fa339_founder_display12_handoff.py
@@ -87,6 +87,8 @@ def test_registry_and_installer_describe_current_founder_surface():
     assert 'die-principal-browser-broker.service die-muxia-dispatch.service' in i
 
 
-def test_graph_frontier_still_waits_for_live_acceptance_seal():
+def test_graph_seals_fa339_and_opens_fa340():
     g=json.loads(GRAPH.read_text(encoding='utf-8')); by={t['id']:t for t in g['tasks']}
     assert by['FA-339']['depends_on']==['FA-338'] and by['FA-338']['status']=='DONE'
+    assert by['FA-339']['status']=='DONE' and by['FA-340']['status']=='READY'
+    assert 'FA-339-founder-visible-display12-handoff.receipt.json' in by['FA-339']['artifact']
